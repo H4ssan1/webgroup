eb218fd6cc261d3fe6487828a6a710b6c90481e5
Mohkhayat <95099609+Mohkhayat@users.noreply.github.com>
2023-12-04 20:56:12 +0000

    Add files via upload
    mk testing

---
+++ models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+#Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=200, unique= True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_pic = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+    #function for admin
+    def str(self):
+        return self.username
