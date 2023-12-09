aa6b61cbd0d92177ea080404cefef67a4ace1115
Hassan Shugaa <h.shugaa@se21.qmul.ac.uk>
2023-12-09 12:22:01 +0000

    profile image works

--- cwgroup/api/models.py
+++ cwgroup/api/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+import datetime
 
 
 # Create your models here.
 class CustomUser(AbstractUser):
     email = models.CharField(max_length=50)
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_pic = models.ImageField(default="default.png", upload_to="profile_pic/")
+    date_of_birth = models.DateField(blank=True, default=datetime.datetime.now)
+    profile_pic = models.ImageField(upload_to="profile_pic/")
 
     def __str__(self):
         return f"{self.id}:{self.username} {self.email}"
--- cwgroup/project/settings.py
+++ cwgroup/project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -126,7 +126,8 @@ AUTH_USER_MODEL = "api.CustomUser"
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
