5265290ce39e61d36b67c17d954a3f41f577c25b
ZubairK1 <95387210+ZubairK1@users.noreply.github.com>
2023-12-13 17:23:54 +0000

    frontend with pinia and router

---
+++ cwgroup/frontend/src/pages/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+
+        <div>profile image</div>
+        <div>
+            <h1>{{ userStore.user.name }}</h1>
+        </div>
+        <div>
+            favourite News categories:
+            <br>
+            <input type="checkbox" id="sport" name="sport" value="sport">
+            <label for="sport">sport</label><br>
+            <input type="checkbox" id="finance" name="finance" value="finance">
+            <label for="finance">finance</label><br>
+            <input type="checkbox" id="World" name="World" value="World">
+            <label for="World">World</label><br>
+        </div>
+
+
+
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent, } from "vue";
+import { useUserStore } from "../userStore";
+
+export default defineComponent({
+    data() {
+        return {
+            title: "Profile Page",
+        };
+    },
+    setup() {
+        const userStore = useUserStore();
+        return { userStore };
+    }
+})
+</script>
+
+
